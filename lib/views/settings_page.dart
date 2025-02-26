1cb9ec3bc2dd9e1725981eb0d1c90b855cdb88fa
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-02-26 19:20:04 -0300

    refactor: página de configurações melhorada

--- lib/views/settings_page.dart
+++ lib/views/settings_page.dart
@@ -7,31 +7,56 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final ThemeProvider themeProvider = context.watch<ThemeProvider>();
     return Scaffold(
-      appBar: AppBar(title: Text('Settings'),),
+      appBar: AppBar(title: Text('Settings')),
       body: Padding(
-        padding: EdgeInsets.all(10),
+        padding: EdgeInsets.all(15),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Theme settings', style: TextStyle(fontSize: 17),),
-            SizedBox(height: 10,),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Theme Mode'),
-                    Switch(
-                      value: themeProvider.isDarkMode, onChanged: (context) => themeProvider.toggleTheme()
+            Text('Theme settings', style: TextStyle(fontSize: 17)),
+            SizedBox(height: 10),
+            Consumer<ThemeProvider>(
+              builder: (context, value, child) {
+                return InkWell(
+                  radius: 15,
+                  onTap: () => value.toggleTheme(),
+                  child: Card.filled(
+                    margin: EdgeInsets.all(0),
+                    // color: Theme.of(context).colorScheme.surfaceContainer,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            spacing: 7,
+                            children: [
+                              Icon(
+                                value.isDarkMode
+                                    ? Icons.dark_mode
+                                    : Icons.light_mode,
+                               size: 20, 
+                              ),
+                              Text(
+                                value.isDarkMode ? 'Dark mode' : 'Light mode',
+                              ),
+                            ],
+                          ),
+                          Switch(
+                            value: value.isDarkMode,
+                            onChanged: (context) => value.toggleTheme(),
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
-                ),
-              ),
+                  ),
+                );
+              },
             ),
-            
           ],
         ),
       ),
