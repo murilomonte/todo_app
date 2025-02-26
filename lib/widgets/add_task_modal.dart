b5743ab3b071b89dcce0852dbbc92d7df81e5d13
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-02-26 10:55:50 -0300

    feat: validação no formulário de tasks

--- lib/widgets/add_task_modal.dart
+++ lib/widgets/add_task_modal.dart
@@ -2,47 +2,95 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_provider/providers/todo_provider.dart';
 
-class AddTaskModal extends StatelessWidget {
+class AddTaskModal extends StatefulWidget {
+  const AddTaskModal({super.key});
+
+  @override
+  State<AddTaskModal> createState() => _AddTaskModalState();
+}
+
+class _AddTaskModalState extends State<AddTaskModal> {
   final TextEditingController _controller = TextEditingController();
-  AddTaskModal({super.key});
 
   @override
   Widget build(BuildContext context) {
     _controller.text = '';
+    // key que identifica unicamente o formulário
+    final formKey = GlobalKey<FormState>();
 
     return Dialog(
       child: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Add task',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-              ),
-              SizedBox(height: 10),
+          padding: EdgeInsets.all(20),
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Add task',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(height: 10),
 
-              TextField(
-                autofocus: true,
-                controller: _controller,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  label: Text('Task name'),
-                ),
-              ),
-              SizedBox(height: 10),
-              Center(
-                child: OutlinedButton(
-                  onPressed: () {
-                    final todo = context.read<TodoProvider>();
-                    todo.addTask(_controller.text);
-                    Navigator.pop(context);
+                TextFormField(
+                  autofocus: true,
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    label: Text('Task name'),
+                  ),
+                  validator: (value) {
+                    // validação que retorna uma mensagem ao usuário
+                    if (value == null || value.isEmpty) {
+                      return 'Add task name';
+                    }
+                    return null;
                   },
-                  child: Text('Add'),
                 ),
-              ),
-            ],
+                SizedBox(height: 10),
+                Row(
+                  spacing: 10,
+                  children: [
+                    Expanded(
+                      child: OutlinedButton.icon(
+                        style: OutlinedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(vertical: 25),
+                        ),
+                        onPressed: () => Navigator.pop(context),
+                        icon: Icon(Icons.close),
+                        label: Text('Close'),
+                      ),
+                    ),
+                    Expanded(
+                      child: FilledButton.icon(
+                        style: FilledButton.styleFrom(
+                          padding: EdgeInsets.symmetric(vertical: 25),
+                        ),
+                        onPressed: () {
+                          // verifica se o campo é válido. Caso contrário, mostra a mensagem de erro
+                          if (formKey.currentState!.validate()) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text('Task added'),
+                                behavior: SnackBarBehavior.floating,
+                                showCloseIcon: true,
+                                duration: Duration(seconds: 2),
+                              ),
+                            );
+                            final todo = context.read<TodoProvider>();
+                            todo.addTask(_controller.text);
+                            Navigator.pop(context);
+                          }
+                        },
+                        icon: Icon(Icons.add),
+                        label: Text('Add'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
