d42277c54510237ddcfa6740525a6bc161823dca
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-04-05 21:36:36 -0300

    Update theme_controller.dart

--- lib/src/controllers/theme_controller.dart
+++ lib/src/controllers/theme_controller.dart
@@ -8,17 +8,17 @@ import 'package:todo_provider/src/theme/light_mode.dart';
 import 'package:todo_provider/src/controllers/user_controller.dart';
 
 class ThemeController extends ChangeNotifier {
-  final UserController _UserController;
+  final UserController _userController;
   ThemeData? _themeData;
 
   ThemeController(BuildContext context)
-    : _UserController = Provider.of<UserController>(context, listen: false) {
+    : _userController = Provider.of<UserController>(context, listen: false) {
     _initializeTheme(context);
   }
 
   Future<void> _initializeTheme(BuildContext context) async {
-    await _UserController.getUser();
-    _themeData = _UserController.user!.theme == 'light' ? lightMode : darkMode;
+    await _userController.getUser();
+    _themeData = _userController.user!.theme == 'light' ? lightMode : darkMode;
     notifyListeners();
   }
 
@@ -29,7 +29,7 @@ class ThemeController extends ChangeNotifier {
   // Toggle Theme
   void toggleTheme() {
     _themeData = _themeData == lightMode ? darkMode : lightMode;
-    _UserController.updateUserTheme(isDarkMode ? 'dark' : 'light');
+    _userController.updateUserTheme(isDarkMode ? 'dark' : 'light');
     notifyListeners();
   }
 }
